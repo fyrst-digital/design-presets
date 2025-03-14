905dddb27826d265aa6c6113d4dbefd248c5b7c2
robjke <robjke@proton.me>
2025-03-14 20:44:17 +0100

    patterns: extend default containers

--- presets.ts
+++ presets.ts
@@ -1,5 +1,5 @@
 import { definePreset } from '@pandacss/dev'
-import { colors, spacing, radii, breakpoints } from './panda/tokens'
+import { colors, spacing, radii, fontSizes, breakpoints } from './panda/tokens'
 import { colors as semanticColors } from './panda/semantic-tokens'
 import { card } from './panda/recipes'
 
@@ -9,7 +9,8 @@ const panda = definePreset({
         tokens: {
             colors: colors,
             spacing: spacing,
-            radii: radii
+            radii: radii,
+            fontSizes: fontSizes
         },
         semanticTokens: {
             colors: semanticColors
@@ -18,6 +19,33 @@ const panda = definePreset({
             breakpoints: breakpoints,
             slotRecipes: {
                 card: card
+            }
+        }
+    },
+    patterns: {
+        extend: {
+            container: {
+                transform({maxWidth, px}) {
+                    return {
+                        position: 'relative',
+                        maxWidth: { 
+                            base: '360px', 
+                            '3xs': '360px', 
+                            '2xs': '400px', 
+                            'xs': '500px', 
+                            'sm': '620px', 
+                            'md': '740px', 
+                            'lg': '1000px', 
+                            'xl': '1200px', 
+                            '2xl': '1400px', 
+                            '3xl': '1600px', 
+                            ...maxWidth 
+                        },
+                        mx: "auto",
+                        px: { base: "lg", ...px },
+                        transition: 'max-width 200ms',
+                    }
+                }
             }
         }
     }
