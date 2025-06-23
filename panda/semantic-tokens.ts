9c45fdaa632056da35c5dbda33477a0623ecd84a
robjke <robjke@proton.me>
2025-06-23 10:03:32 +0200

    tokens: add lucid colors

--- panda/semantic-tokens.ts
+++ panda/semantic-tokens.ts
@@ -16,4 +16,14 @@ export const colors = defineSemanticTokens.colors({
     primary: {
         value: { base: '{colors.green.pale}', _light: '{colors.green.day}' }
     },
+    neutral: {
+        value: { base: '{colors.white}', _light: '{colors.grey.night}' }
+    },
+    lucid: {
+        50: { value: { base: 'rgba(255, 255, 255, 0.1)', _light: 'rgba(0, 0, 0, 0.1)' } },
+        100: { value: { base: 'rgba(255, 255, 255, 0.175)', _light: 'rgba(0, 0, 0, 0.175)' } },
+        200: { value: { base: 'rgba(255, 255, 255, 0.25)', _light: 'rgba(0, 0, 0, 0.25)' } },
+        400: { value: { base: 'rgba(255, 255, 255, 0.5)', _light: 'rgba(0, 0, 0, 0.5)' } },
+        600: { value: { base: 'rgba(255, 255, 255, 0.75)', _light: 'rgba(0, 0, 0, 0.75)' } },
+    }
 })
